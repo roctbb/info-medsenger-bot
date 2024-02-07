cdc8e58d484cc2b7b417fb4681e21ee95195b414
roctbb <roctbb@gmail.com>
2024-02-07 15:01:55 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo pip3 install -r requirements.txt
+sudo cp info.ini /etc/uwsgi/apps/
 sudo cp agents_info.conf /etc/supervisor/conf.d/
 sudo cp agents_info_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
